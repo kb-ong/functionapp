ff5043e540686f16e874cfa6239628337936c696
Ubuntu <ubuntu@ip-172-31-4-167.ap-southeast-2.compute.internal>
2024-01-03 02:09:20 +0000

    first commit

---
+++ src/main/java/com/example/test/Function.java
@@ -0,0 +1,73 @@
+package com.example.test;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+    /**
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     */
+
+    @FunctionName("TriggerStringPost")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req",
+                    methods = {HttpMethod.POST},
+                    authLevel = AuthorizationLevel.ANONYMOUS)
+            HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+
+        // Item list
+        context.getLogger().info("Request body is: " + request.getBody().orElse(""));
+
+        // Check request body
+        if (!request.getBody().isPresent()) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                    .body("Document not found.")
+                    .build();
+        }
+        else {
+            // return JSON from to the client
+            // Generate document
+            final String body = request.getBody().get();
+            final String jsonDocument = "{\"id\":\"123456\", " +
+                    "\"description\": \"" + body + "\"}";
+            return request.createResponseBuilder(HttpStatus.OK)
+                    .header("Content-Type", "application/json")
+                    .body(jsonDocument)
+                    .build();
+        }
+    }
+//    @FunctionName("HttpExample")
+//    public HttpResponseMessage run(
+//            @HttpTrigger(
+//                name = "req",
+//                methods = {HttpMethod.GET, HttpMethod.POST},
+//                authLevel = AuthorizationLevel.ANONYMOUS)
+//                HttpRequestMessage<Optional<String>> request,
+//            final ExecutionContext context) {
+//        context.getLogger().info("Java HTTP trigger processed a request.");
+//
+//        // Parse query parameter
+//        final String query = request.getQueryParameters().get("name");
+//        final String name = request.getBody().orElse(query);
+//
+//        if (name == null) {
+//            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+//        } else {
+//            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+//        }
+//    }
+}
